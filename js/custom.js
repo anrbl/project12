62d69386ab94405b12cbc023262b5fdc745e7309
anrbl <133613567+anrbl@users.noreply.github.com>
2023-08-03 18:20:19 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,24 +4,48 @@ $(function () {
         scrollHorizontally: true,
         scrollOverflow: true,
         scrollingSpeed: 700,
+
+        navigation: false,
+        css3: false,
+        responsiveWidth: 768,
+        responsiveHeight: 800,
+
         onLeave: function (idx, nidx, dir) {
             console.log(idx, nidx)
             if (nidx == '2') {
                 $('.header').addClass('on')
+                $('.side').addClass('on')
             } else if (nidx == '3') {
                 $('.header').addClass('on')
+                $('.side').addClass('on')
             } else if (nidx == '5') {
                 $('.header').addClass('on')
+                $('.side').removeClass('on')
             } else {
                 $('.header').removeClass('on')
+                $('.side').removeClass('on')
             }
+            $('.side_nav li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
         }
+
+
     });
 
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        $('.mv_slidenum li').eq(0).addClass('on');
+        $('.mv_slidenum li').eq(c).addClass('on').siblings().removeClass('on');
+    })
     $('.main_slide').slick({
         arrows: false,
         fade: true,
-        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 700,
+        pauseOnFocus: false,
+        pauseOnHover: false,
     });
     $('.mv_arrow .left').on('click', function () {
         $('.main_slide').slick('slickPrev');
@@ -29,6 +53,11 @@ $(function () {
     $('.mv_arrow .right').on('click', function () {
         $('.main_slide').slick('slickNext');
     })
+    $('.mv_slidenum li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $('.main_slide').slick('slickGoTo', idx);
+    });
 
 
     $('.text_loop').slick({
@@ -61,6 +90,12 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        }],
     });
     $('.bou_arrow .left').on('click', function () {
         $('.bouquet_slide').slick('slickPrev');
@@ -69,10 +104,59 @@ $(function () {
         $('.bouquet_slide').slick('slickNext');
     })
 
-    $('.collection_page li').on('click', function (e) {
-        e.preventDefault();
-        let idx = $(this).index();
-        $('.collection_page li').eq(idx).addClass('on').siblings().removeClass('on');
-        $('.collection .theme').eq(idx).addClass('on').siblings().removeClass('on');
-    })
+
+
+    var length = $(".sec3 .swiper-slide").length;
+    var bullet = ['Jane', 'Taylor', 'Millie'];
+    const Trend = new Swiper('.collection_slide', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        speed: 700,
+        mousewheel: true,
+        pagination: {
+            el: '.collection_page',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<div class="' + className + '"><span>' + (bullet[index]) + '</span></div>';
+            }
+        },
+        navigation: {
+            nextEl: '.col_arrows .right',
+            prevEl: '.col_arrows .left',
+        },
+        on: {
+            slideChange: function () {
+                var idx = this.activeIndex;
+
+                if (idx != 0 && idx != length) $.fn.fullpage.setAllowScrolling(false);
+                if (length == 1 && idx == 0) $.fn.fullpage.setAllowScrolling(false);
+            },
+            slideChangeTransitionEnd: function () {
+                var idx = this.activeIndex;
+                if (idx == 0 || idx >= length - 1) $.fn.fullpage.setAllowScrolling(true);
+            },
+        }
+    });
+
+
+    $('.side_nav li').on('click', function () {
+        $(this).addClass('on').siblings().removeClass('on')
+    });
+
+
+    //반응형
+    $('.mobile_menu').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+        $('h1').toggleClass('on');
+    });
+    $(window).on('resize', function () {
+        $('.mobile_menu').removeClass('on');
+        $('.gnb').removeClass('on');
+    });
+    $('.gnb').on('wheel', function (e) {
+        if ($(this).hasClass('on')) {
+            e.preventDefault();
+        }
+    });
 });
